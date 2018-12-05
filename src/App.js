9330a70fb0bb975da00b8afdbc06039bd885c3fc
john-raber <jrab224@gmail.com>
2018-12-04 20:06:50 -0500

    adding components to profile scene

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import HeaderMenu from "./components/HeaderMenu/index";
 import Login from "./scenes/Login/index";
 import About from "./scenes/About/index";
+import Profile from "./scenes/Profile/index";
 
 import { getCurrentUserFromToken } from "./scenes/Login/services/currentUser/actions/index";
 
@@ -21,6 +22,7 @@ class App extends Component {
         <Switch>
           <Route path="/about" component={About} />
           <Route path="/login" component={Login} />
+          <Route path="/profile" component={Profile} />
         </Switch>
       </div>
     );
--- src/scenes/Profile/index.js
+++ src/scenes/Profile/index.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Container, Row, Col, Jumbotron } from "reactstrap";
 
 import "./Profile.css";
 
 const Profile = () => {
-  return <div />;
+  return (
+    <Container>
+      <Row>
+        <Col md={{ size: 6 }}>
+          <Jumbotron>
+            <h4 className="display-4">Guides</h4>
+          </Jumbotron>
+        </Col>
+        <Col md={{ size: 6 }}>
+          <Jumbotron>
+            <h4 className="display-4">Trainers</h4>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Profile;
