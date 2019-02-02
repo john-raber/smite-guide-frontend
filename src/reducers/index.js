f0727a088016f3eda733e14126f7461890f41555
john-raber <jrab224@gmail.com>
2019-02-01 19:32:03 -0500

    adding initial state for gods to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import currentUserReducer from "../scenes/Login/services/currentUser/reducer";
+import godsReducer from "../scenes/Gods/services/gods/reducer";
 
 const rootReducer = combineReducers({
-  currentUser: currentUserReducer
+  currentUser: currentUserReducer,
+  gods: godsReducer
 });
 
 export default rootReducer;
