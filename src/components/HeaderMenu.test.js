a625ce898d347e754c6d6e73b1c503ed0aefadee
john-raber <jrab224@gmail.com>
2019-02-03 14:12:27 -0500

    eslint formatting update

--- src/components/HeaderMenu.test.js
+++ src/components/HeaderMenu.test.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { shallow } from "enzyme";
+import React from 'react';
+import { shallow } from 'enzyme';
 
-import { HeaderMenu } from "./HeaderMenu/index";
+import { HeaderMenu } from './HeaderMenu/index';
 
-describe("HeaderMenu", () => {
-  it("renders correctly", () => {
-    let wrapper = shallow(<HeaderMenu />);
+describe('HeaderMenu', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(<HeaderMenu />);
 
-    expect(wrapper.find(".headerMenu").length).toEqual(1);
+    expect(wrapper.find('.headerMenu').length).toEqual(1);
   });
 
-  it("matches its snapshot when a currentUser is set", () => {
-    let user = { email: "john@example.com" };
-    let wrapper = shallow(<HeaderMenu currentUser={user} />);
+  it('matches its snapshot when a currentUser is set', () => {
+    const user = { email: 'john@example.com' };
+    const wrapper = shallow(<HeaderMenu currentUser={user} />);
 
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("matches its snapshot when no currentUser is set", () => {
-    let wrapper = shallow(<HeaderMenu />);
+  it('matches its snapshot when no currentUser is set', () => {
+    const wrapper = shallow(<HeaderMenu />);
 
     expect(wrapper).toMatchSnapshot();
   });
